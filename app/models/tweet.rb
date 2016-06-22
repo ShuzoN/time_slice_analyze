51510f0d6cec47682542e8176cfc2c90a579623b
ShuzoN <d.namu.r21@gmail.com>
2016-06-22 15:13:28 +0900

    upd: models/tweet, DBに取得Tweetを記録する機能を追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,27 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :user_id, :text, :post_date
+  validates :twitter_id, numericality: {only_integer: true}, uniqueness:true 
+
+  # TweetをDBに追加するメソッド
+  def self.store_db(tweets) #tw:Hash
+
+    if tweets.size==0 || tweets==nil
+      return puts "no data! (Tweet::store_db)" 
+    end
+
+
+    tweets.each do |tw|
+      twitter_id        = tw["id"]
+      user_twitter_id   = tw["user"]["id"]
+      user = User.find_by( twitter_id: user_twitter_id)
+
+      Tweet.where(twitter_id: twitter_id).first_or_create(
+        user_id:   user.id,
+        text:      tw["text"],
+        post_date: Time.zone.parse(tw["created_at"]).to_s(:db)
+      )
+    end
+  end
 end
--- lib/assets/main.rb
+++ lib/assets/main.rb
@@ -7,16 +7,14 @@ class Main
     @crawler = Assets::Crawler.new
   end
 
-
   def main
-  # TwitterAPIから1ユーザのtweetを指定件数 取得
+    # TwitterAPIから1ユーザのtweetを指定件数 取得
     tweets = @crawler.get_tweets(10)
 
+    # DBに取得データを保存
     user = tweets.first["user"]
     User.store_db(user)
-
-    # tweet モデルがまだない
-    # @crawler.store_tweets_to_db(tweets)
+    Tweet.store_db(tweets)
   end
 end
 
