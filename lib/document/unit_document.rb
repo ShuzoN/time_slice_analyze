d3cc12665e0033ffb23b3bc5454149b69d7d4b8e
ShuzoN <d.namu.r21@gmail.com>
2016-07-05 15:18:36 +0900

    mod: document/unit, 単一文書に含まれる単語総数を数える機能を追加

--- lib/document/unit_document.rb
+++ lib/document/unit_document.rb
@@ -5,12 +5,14 @@ class Document::UnitDocument < Document::Document
   def initialize(org_t="")
     # この文書中に含まれる単語の出現頻度を単語ごとに記録
     @nouns_frequency_dic=Hash.new(0) #{:noun => frequency_in_doc}
+    @num_all_words=0
     super(org_t)
   end
 
   # 文書中に含まれる名詞の出現頻度を名詞ごとに記録
   def count_nouns_frequency
     @@mecab.parse(org_txt) {|word| 
+      @num_all_words+=1
       next unless (41..47) === word.posid || 38 == word.posid
       next if word.surface=~/\w/
       @nouns_frequency_dic[word.surface.to_sym] += 1
