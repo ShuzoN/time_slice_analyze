06a7ddf22e7f0e8f352cdd4b95ac4d4418e73022
ShuzoN <d.namu.r21@gmail.com>
2016-07-13 15:41:17 +0900

    wip: 複数の16進数からなる絵文字を整形する機能を追加

--- lib/assets/test.rb
+++ lib/assets/test.rb
@@ -4,22 +4,33 @@ Bundler.require
 class FormatEmojiDic
   def main
     # 絵文字の辞書ファイルを開く
-    emoji_dic_location = \
-      File.expand_path("../../../config/dictionary/emoji_data.txt", __FILE__)
-    emoji_org_dic = File.open(emoji_dic_location, "r").read
+    emoji_dat_locate = File.expand_path(
+      "../../../config/dictionary/emoji_data.txt", __FILE__)
+    emoji_seq_locate = File.expand_path(
+      "../../../config/dictionary/emoji_sequences.txt", __FILE__)
+    emoji_zwj_locate = File.expand_path( 
+      "../../../config/dictionary/emoji_zwj_sequences.txt", __FILE__)
+
+    emoji_data_dic = File.open(emoji_dat_locate, "r").read
+    emoji_seq_dic  = File.open(emoji_seq_locate, "r").read
+    emoji_zwj_dic  = File.open(emoji_zwj_locate, "r").read
 
     # 絵文字辞書(unicode codepoint:16進数)
     emoji_dic = []
+
     # 1つの16進数からなる絵文字の辞書を生成
-    emoji_dic = format_emoji_dic_for_data(emoji_org_dic)
+    emoji_dic =  format_emoji_dic_for_data(emoji_data_dic)
+
+    # 複数の16進数からなる絵文字の辞書を生成
+    emoji_dic << format_emoji_dic_for_seq(emoji_seq_dic)
+    emoji_dic << format_emoji_dic_for_seq(emoji_zwj_dic)
+    
   end
 
   # 1つの16進数からなる絵文字の辞書を生成
-  def format_emoji_dic_for_data(emoji_org_dic)
-    # 絵文字の辞書(unicode codepoint:16進数)
-    dic = []
-
-    emoji_org_dic.lines.each do |emoji|
+  def format_emoji_dic_for_data(emoji_data_dic)
+    dic = [] # 絵文字の辞書(unicode codepoint:16進数)
+    emoji_data_dic.lines.each do |emoji|
       # 辞書から絵文字のUnicodeを取得
       emoji_unicode = emoji.split(/;/)[0].strip
       break if emoji_unicode == "EOF"
@@ -32,7 +43,18 @@ class FormatEmojiDic
       elsif dic << emoji_unicode
       end
     end
-    dic.flatten
+    dic.flatten!
+  end
+
+  # 複数の16進数からなる絵文字の辞書を生成
+  def format_emoji_dic_for_seq(emoji_seq_dic)
+    dic = [] # 絵文字の辞書(unicode codepoint:16進数)
+    emoji_seq_dic.lines.each do |emoji|
+      emoji_unicode = emoji.split(/;/)[0].strip
+      break if emoji_unicode == "EOF"
+      dic << emoji_unicode
+    end
+    dic
   end
 
   # 絵文字がカテゴリでまとめてRangeで表現されている場合がある
