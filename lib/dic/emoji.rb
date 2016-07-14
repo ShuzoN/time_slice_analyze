866514c4421a53fc4539a855a24487e0a3b05cff
ShuzoN <d.namu.r21@gmail.com>
2016-07-14 14:26:44 +0900

    mod: 絵文字を除去する機能を実装

--- lib/dic/emoji.rb
+++ lib/dic/emoji.rb
@@ -11,16 +11,22 @@ class Dic::Emoji
   def generate_emoji_dic
     dic = [""]
     # 辞書ファイルの指定
-    emoji_dic_location = \
-      File.expand_path("../../../config/dictionary/emoji_data.txt", __FILE__)
+    emoji_dic_location = File.expand_path(
+      "../../../config/dictionary/formated_emoji.txt", __FILE__)
     emoji_txt = File.open(emoji_dic_location, "r").read
 
     # 辞書の生成
-    emoji_txt.each_line do |em|
-      next  if em.chr.nil? || em.chr == ""
-      break if em.chr == "EOF".chr
-      dic << em.split(";")[0].strip.to_sym
-    end
+    dic = emoji_txt.split(/,/)
     dic
+  end
+
+  # 単語をutf8の文字コードに変換する(U+は省略)
+  def self.convert_utf_code(word)
+    utf_code_point = word.unpack("U*") # array
+    utf_code = ""
+    utf_code_point.each do |cp|
+      utf_code << cp.to_s + " "
+    end
+    utf_code.strip
   end
 end
--- lib/document/unit_document.rb
+++ lib/document/unit_document.rb
@@ -12,13 +12,17 @@ class Document::UnitDocument < Document::Document
   def count_nouns_frequency
     @@mecab.parse(org_txt) do |word|
       @num_all_words += 1
-      next unless (41..47) === word.posid || 38 == word.posid
+      posid   = word.posid
+      surface = word.surface
+
+      # 特定の名詞のみを対象(一般, 固有名詞)
+      next unless (41..47) === posid || 38 == word.posid
       # 半角英数の除去(全角 -> 半角英数変換)
-      next if NKF.nkf("-m0Z1 -W -w", word.surface) =~ /\w/
+      next if NKF.nkf("-m0Z1 -W -w", surface) =~ /\w/
       # 絵文字の除去
-      # next if @@emoji.dic.include?(word.surface.to_i(16))
-      # puts word.surface.pack("U*").each{|word_cp| word_cp.to_i(16)}
-      @nouns_frequency_dic[word.surface.to_sym] += 1
+      utf_code = Dic::Emoji.convert_utf_code(surface)
+      next if @@emoji.dic.include?(utf_code)
+      @nouns_frequency_dic[surface.to_sym] += 1
     end
   end
 end
