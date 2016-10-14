d8b384b5392ed7b3492eaf7055fab6f03a8e4593
ShuzoN <d.namu.r21@gmail.com>
2016-10-14 22:57:44 +0900

    add assets/jaro_winkler_test: ジェロウィンクラー距離を求める機能を追加

---
+++ lib/assets/jaro_winkler_test.rb
@@ -0,0 +1,60 @@
+require "bundler"
+Bundler.require
+require "nkf"
+
+class JaroWinklerDistance
+  
+  def initialize
+    @mecab = Natto::MeCab.new(dicdir: "/usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd")
+
+  end
+
+  # 文字列配列に対して, 
+  # ジャロウィンクラー距離を求める
+  def test
+    str = ["大阪市長選挙","大阪ダブル選挙","市岡中学校","大阪府知事選挙","吉村洋文","期日前投票","ひろふみ","選挙結果","法定","拡散希望"]
+
+    calc_distance(str)
+    # absorption_hiragana(str[6])
+  end
+
+  def to_katakana(str)
+    NKF.nkf('-w -h2 --katakana', str)
+  end
+
+  def absorption_hiragana(str)
+
+    @mecab.parse(str) do |word|
+      p word.surface
+      p word.feature.split(/,/)[7]
+    end
+
+    puts to_katakana(str)
+
+  end
+
+  # 文字列配列の全要素に対して, 
+  # ジャロウィンクラー距離を求める
+  # ジャロウィンクラー距離 > 0 の場合のみ表示
+  def calc_distance(words=[])
+    return if words==[]
+
+    words.each_with_index do |w1, idx|
+      words.drop(idx).each_with_index do |w2, jdx|
+        jdx += idx
+        next if jdx == idx
+
+        
+        distance = JaroWinkler.distance(w1,w2)
+
+        if distance > 0
+          puts "#{w1} : #{w2}\n score : #{distance}"
+        end
+      end
+    end
+
+  end
+end
+
+JaroWinklerDistance.new.test
+
