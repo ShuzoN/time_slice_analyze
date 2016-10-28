f45336fbca541b4129a70775f66771c4846d9dc2
shuzon2 <d.namu.r210@ezweb.ne.jp>
2016-10-28 15:20:14 +0900

    [upd] leveldb.rb, TFIDF値を保存するKVSをclassとして実装.

--- lib/test/leveldb.rb
+++ lib/test/leveldb.rb
@@ -3,55 +3,74 @@ require "bundler"
 Bundler.require
 require 'leveldb'
 
+class TfidfValueDB
 
-@db = LevelDB::DB.new("./lib/test/testdb")
+  # ユーザ名に合わせたKVSDBの作成
+  def initialize(username)
+    begin
+      # 引数の型チェック
+      raise ArgumentError if !username.kind_of?(String)
 
-# dbにKey:string => Value:Hashの形で記録する
-# leveldbは, 文字列のみ記録可能
-# hashを文字列に変換して記録する
-def put(orgkey, value_hash)
-  begin 
-    key = orgkey
+      dbpath = "./lib/test/" + username
+      @db = LevelDB::DB.new(dbpath)
 
-    if !orgkey.kind_of?(String)
-      raise ArgumentError, "type error. require String. #{key.class}" 
-    elsif !value_hash.kind_of?(Hash)
-      raise ArgumentError, "type error. require Hash. #{value_hash.class}" 
+    rescue ArgumentError=> ex
+      puts __FILE__ + "  " + ex.inspect 
+      exit 1
     end
-
-    @db[key] = value_hash.to_s
-
-  rescue ArgumentError=> ex
-     puts __FILE__ + "  " + ex.inspect 
-    exit 1
   end
-end
 
-# dbからKeyに対応するvalueを取得
-# 文字列からHashを復元する
-# 戻り値 : Hash
-def get(key)
-  begin 
-    if !key.kind_of?(String)
-      raise ArgumentError, "type error. require String. #{key.class}" 
+  # dbにKey:string => Value:Hashの形で記録する
+  # leveldbは, 文字列のみ記録可能
+  # hashを文字列に変換して記録する
+  def put(org_key, value_hash)
+    begin 
+      key = org_key
+
+      if !org_key.kind_of?(String)
+        raise ArgumentError, "type error. require String. #{key.class}" 
+      elsif !value_hash.kind_of?(Hash)
+        raise ArgumentError, "type error. require Hash. #{value_hash.class}" 
+      end
+
+      @db[key] = value_hash.to_s
+
+    rescue ArgumentError=> ex
+      puts __FILE__ + "  " + ex.inspect 
+      exit 1
     end
+  end
 
-    # dbから値の取得
-    result_value = @db[key]
+  # dbからKeyに対応するvalueを取得
+  # 文字列からHashを復元する
+  # 戻り値 : Hash
+  def get(key)
+    begin 
+      if !key.kind_of?(String)
+        raise ArgumentError, "type error. require String. #{key.class}" 
+      end
 
-    raise KeyError, "#{key} has no value." unless result_value
+      # dbから値の取得
+      result_value = @db[key]
+
+      raise KeyError, "#{key} has no value." unless result_value
 
 
-    # dbから受け取ったデータをString -> Hash変換
-    result_hash = eval(result_value)
+      # dbから受け取ったデータをString -> Hash変換
+      result_hash = eval(result_value)
 
-    raise TypeError unless result_hash.kind_of?(Hash)
+      raise TypeError unless result_hash.kind_of?(Hash)
 
-    return result_hash
+      return result_hash
 
-  rescue ArgumentError=> ex
-    puts __FILE__ + "  " + ex.inspect 
-    exit 1
+    rescue ArgumentError=> ex
+      puts __FILE__ + "  " + ex.inspect 
+      exit 1
+    end
+  end
+
+  def keys
+    @db.keys
   end
 end
 
@@ -68,7 +87,12 @@ strings.each.with_index do |key, sidx|
 end
 
 
-p @db.keys
+db = TfidfValueDB.new("testdb")
+db.put("1", test_hash)
+p db.get("1")
 
-# put("1", test_hash)
-p get("1")
+p db.keys
+
+
+
+
