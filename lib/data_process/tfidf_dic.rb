e10e6edb2b4b13e5a6a51f6518f6dd76490b8c0a
shuzon2 <d.namu.r210@ezweb.ne.jp>
2016-10-31 17:40:37 +0900

    [upd] csvを読む関数を定義

--- lib/data_process/tfidf_dic.rb
+++ lib/data_process/tfidf_dic.rb
@@ -1,16 +1,21 @@
 require 'pathname'
 
-begin 
-  csv_filename = "tfidf_result_hashimoto_100.csv"
-  csv_filepath = "tmp/#{csv_filename}" 
-  f_path = Pathname.new(csv_filepath.to_s)
-  raise IOError, "file not exist." unless f_path.cleanpath.file?
-  f = File.open(f_path,'r:UTF-16LE:UTF-8')
-  content = f.read()
-  f.close
+# csvファイルを読み込んで, 文字列を返す
+def read_csv(filename)
+  begin
+    # 引数で渡されたファイルパスを基に
+    # ファイルオープン
+    csv_filepath = "#{filename}"
+    raise ArgumentError if !filename.kind_of?(String)
+    f_path = Pathname.new(csv_filepath)
+    raise IOError, "file not exist. enter filepath : #{f_path}" unless f_path.cleanpath.file?
 
-  puts content
-
-rescue IOError => e
-  e.inspect
+    # CSVがUTF-16で保存されているのでUTF8に変換
+    f = File.open(f_path,'r:UTF-16LE:UTF-8')
+    content = f.read()
+    f.close
+    return content
+  rescue => e
+    puts e.inspect; puts $@; exit 1
+  end
 end
