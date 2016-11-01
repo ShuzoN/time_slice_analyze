16cd9c3c04fedb9cd4a8aeb9591c39fdc45d9911
shuzon2 <d.namu.r210@ezweb.ne.jp>
2016-11-01 12:48:12 +0900

    [upd] cut_intervalメソッドに例外処理追加とバグ修正
     * csvを配列にするときに, BOMが配列の先頭に置かれる(Stringでは空文字扱い)
      - これまでは, Array#Rotateで一番後ろに位置を変えていた
      - Array#Rotateを使わずBOMを削除する処理に変更
      - 例外処理を通過するようにした

--- lib/data_process/tfidf_dic.rb
+++ lib/data_process/tfidf_dic.rb
@@ -21,20 +21,34 @@ def read_csv(filename)
 end
 
 # 区間ごとの内容を切り出す
-# Key: 区間 , Value : 単語=>tfidfのhash
+# 区間=>{単語=>tfidf}という2段組のHashを生成する
+
 def cut_interval(content)
+  begin
     csv_content = content
     raise ArgumentError if !content.kind_of?(String)
 
-    separeted_details = csv_content.split(/(\d{1,4}:\d{1,4})/).rotate
+    # 区間ごとにcsvを切り出す
+    # ["0:100", ",ゼミ,0.014461107650\n,メルマガ,0.0124740051308\n,..."]
+    separeted_interval = csv_content.split(/(\d{1,4}:\d{1,4})/)
+    raise RangeError unless separeted_interval.delete_at(0) # BOM削除
 
+    # 区間と単語群(表記,tfidf値)からなる配列から
+    # 区間=>{単語=>tfidf}という2段組のHashを生成する
     interval_words_tfidf={}
-    separeted_details.each_slice(2) do |range, values|
-      break if !range.kind_of?(String) || !values.kind_of?(String)
+    separeted_interval.each_slice(2) do |range, values|
+      if !range.kind_of?(String) || !values.kind_of?(String)
+        raise StopIteration, \
+          "<key:#{range}, #{range.class}>, <value:#{values}, #{values.class}>"
+      end
       word_tfidf = values.scan(/,([^,.]*),(\d.\d+)\n/).to_h
       interval_words_tfidf.store(range, word_tfidf)
     end
     return interval_words_tfidf
+
+  rescue => e
+    puts e.inspect; puts $@; exit 1
+  end
 end
 
 # 単語が含まれている区間を検索する
@@ -48,7 +62,7 @@ end
 
 
 # 単語が含まれている区間を検索し, 
-# 区間とtfidf値の対で取得する
+# 区間とtfidf値の対を返す
 # [[区間1, tfidf値],[区間2, tfidf値],...]
 def value_assoc(keyword, seperated_interval)
   resultset= []
@@ -64,5 +78,5 @@ end
 filename = "tmp/tfidf_result_hashimoto_100.csv"
 tfidf_result = read_csv(filename)
 seperated_interval = cut_interval(tfidf_result)
-p included_interval("党大会", seperated_interval)
-p value_assoc("党大会", seperated_interval)
+p included_interval("メルマガ", seperated_interval)
+p value_assoc("メルマガ", seperated_interval)
