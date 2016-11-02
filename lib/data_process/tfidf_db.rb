167c69f9bcbe62ed19d23999f286d0d02049477b
shuzon2 <d.namu.r210@ezweb.ne.jp>
2016-11-02 20:13:03 +0900

    [del&upd] tfidfdbにcsvからdbにデータを挿入する機能を追加

--- lib/data_process/tfidf_db.rb
+++ lib/data_process/tfidf_db.rb
@@ -1,101 +1,106 @@
 
 require "bundler"
 Bundler.require
-require 'leveldb'
+require "leveldb"
 
-class DataProcess::TfidfDB
-
-  # ユーザ名に合わせたKVSDBの作成
-  def initialize(username:"hashimoto", interval: 100 )
-    begin
+module DataProcess
+  # {単語=>{区間=>tfidf値}でtfidf値を記録するDB
+  class TfidfDB
+    # ユーザ名に合わせたKVSDBの作成
+    def initialize(username: "", interval: 0)
       # 引数の型チェック
-      raise ArgumentError if !username.kind_of?(String)
-      raise ArgumentError if !interval.kind_of?(Numeric)
-
+      raise ArgumentError if !username.is_a?(String)  || username.empty?
+      raise ArgumentError if !interval.is_a?(Integer) || interval.zero?
+      @username = username
+      @interval = interval
       dbpath = "./tmp/" + username + "_" + interval.to_s
       @db = LevelDB::DB.new(dbpath)
-    rescue ArgumentError=> ex 
-      puts $@ + "  " + ex.inspect 
+    rescue ArgumentError => e
+      puts e.inspect
+      puts $@
       exit 1
     end
-  end
 
-  # dbにKey:string => Value:Hashの形で記録する
-  # leveldbは, 文字列のみ記録可能
-  # hashを文字列に変換して記録する
-  def put(org_key, value_hash)
-    begin 
-      key = org_key
-      raise ArgumentError if !org_key.kind_of?(String)
-      raise ArgumentError if !value_hash.kind_of?(Hash)
-
-      @db[key] = value_hash.to_s
-
-    rescue ArgumentError=> ex
-      puts $@ + "  " + ex.inspect 
+    # dbにKey:string => Value:Hashの形で記録する
+    # leveldbは, 文字列のみ記録可能
+    # hashを文字列に変換して記録する
+    def put(key, interval_value)
+      raise ArgumentError unless key.is_a?(String)
+      raise ArgumentError unless interval_value.is_a?(Hash)
+      @db[key] = interval_value.to_s
+    rescue ArgumentError => e
+      puts e.inspect
+      puts $@
       exit 1
     end
-  end
 
-  # dbからKeyに対応するvalueを取得
-  # 文字列からHashを復元する
-  # 戻り値 : Hash
-  def get(key)
-    begin 
-      raise ArgumentError if !key.kind_of?(String)
-
+    # dbからKeyに対応するvalueを取得
+    # 文字列からHashを復元する
+    # 戻り値 : Hash
+    def get(key)
+      raise ArgumentError unless key.is_a?(String)
       # dbから値の取得
       result_value = @db[key]
-
       raise KeyError unless result_value
-
 
       # dbから受け取ったデータをString -> Hash変換
       result_hash = eval(result_value)
-
-      raise TypeError unless result_hash.kind_of?(Hash)
+      raise TypeError unless result_hash.is_a?(Hash)
 
       return result_hash
-
-    rescue ArgumentError=> ex
-      puts $@ + "  " + ex.inspect 
+    rescue ArgumentError => e
+      puts e.inspect
+      puts $@
       exit 1
     end
-  end
 
-  def delete(key)
-    begin
-      raise ArgumentError if !key.kind_of?(String)
+    def delete(key)
+      raise ArgumentError unless key.is_a?(String)
       @db.delete(key)
-    rescue ArgumentError=> ex
-      puts $@ + "  " + ex.inspect
+    rescue ArgumentError => e
+      puts e.inspect
+      puts $@
       exit 1
     end
-  end
 
-  def keys
-    @db.map do |k, v| k.force_encoding("utf-8") end
-  end
+    def keys
+      @db.map { |k, _| k.force_encoding("utf-8") }
+    end
 
-  def values
-    @db.values
+    def values
+      @db.values
+    end
+
+    # csvファイルからKVSにTFIDFデータを記録する
+    # {単語=>{区間=>tfidf}}
+    # {"党大会"=>{"0:100"=>0.000234, "100:200"=>0.000452}, ...}
+    def store_db_from_csv(filename)
+      conv_csv_tfidf = DataProcess::ConvCsvToTfidf.new
+      # csvを読み込み
+      tfidf_csv = conv_csv_tfidf.read_csv(filename)
+      # 区間ごとに区切る
+      seperated_interval = conv_csv_tfidf.cut_interval(tfidf_csv)
+
+      # 全区間の全単語を取得
+      all_words = conv_csv_tfidf.words(seperated_interval)
+
+      # {単語=>{区間=>tfidf}}の形でデータを整形
+      words_separated_interval = {}
+      all_words.flatten.uniq.each do |word|
+        interval_tfidf = conv_csv_tfidf.value_assoc(word, seperated_interval).to_h
+        words_separated_interval.store(word, interval_tfidf)
+      end
+
+      # KVSに{単語=>{区間=>tfidf}}の形でデータを保存
+      words_separated_interval.each do |key, interval_value|
+        put(key, interval_value)
+      end
+    end
   end
 end
 
 # usage
-# keywords = ["バイブル", "テクノロジ", "サイクル", "選挙"]
-# word1_tfidf = {"100:200"=>0.0023, "200:300"=>0.00356, "300:400"=>0.00456}
-# word2_tfidf = {"100:200"=>0.0223, "200:300"=>0.02252, "300:400"=>0.01456}
-# word3_tfidf = {"100:200"=>0.0531, "200:300"=>0.00450, "300:400"=>0.00562}
-# word4_tfidf = {"100:200"=>0.0823, "200:300"=>0.00474, "300:400"=>0.04456}
-# value = [word1_tfidf,word2_tfidf,word3_tfidf,word4_tfidf]
-#
-# db = TfidfValueDB.new("testdb")
-#
-# keywords.each.with_index do |key, sidx|
-#   db.put(key, value[sidx])
-# end
-#
-# p db.get("サイクル")
-# p db.keys
-# p db.values
+# filename = "tmp/tfidf_result_hashimoto_100.csv"
+# d = DataProcess::TfidfDB.new(username: "hashimoto", interval: 100)
+# d.store_db_from_csv(filename)
+
